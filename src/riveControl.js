5aaf1cb22c58ecb94fbe9f77c2dd6a98dd515ad1
dlsubzh <joshua.binswanger@gmail.com>
2024-06-30 17:26:07 +0200

    make video stop on close

--- src/riveControl.js
+++ src/riveControl.js
@@ -42,11 +42,22 @@ function cleanupRive (){
  console.log("resizeDrawingSurfacetoCanvas done")
 
 
-  function hideVid() {
-    const popupContent = document.getElementsByClassName("popup");
-    for (let i = 0; i < popupContent.length; i++) {
-        popupContent[i].style.visibility = "hidden";
-    }
+  function hideVid(id) {
+    let containerElement = document.getElementById(id);
+    containerElement.style.visibility = "hidden";
+  }
+  
+  function videoStopper(id) {
+      let containerElement = document.getElementById(id);
+      let iframe_tag = containerElement.querySelector( 'iframe');
+      let video_tag = containerElement.querySelector( 'video' );
+      if ( iframe_tag) {
+          let iframeSrc = iframe_tag.src;
+          iframe_tag.src = iframeSrc; 
+      }
+      if ( video_tag) {
+          video_tag.pause();
+      }
 
     
 }
